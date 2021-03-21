289f6d85b2cf645208f7df38b39d134d682102e6
Sonamdema123 <sdema1684@gmail.com>
2021-03-21 23:27:04 +0600

    TODO_4

---
+++ Lab/TODO_4/app/src/main/java/com/example/todo_4/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.todo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.todo_4";
+    public static final int TEXT_REQUEST = 1;
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
+    }
+
+    public void next(View view) {
+    Intent obj = new Intent(this, MainActivity2.class);
+            String message = mMessageEditText.getText().toString();
+            obj.putExtra(EXTRA_MESSAGE, message);
+            startActivityForResult(obj, TEXT_REQUEST);
+
+    }
+}
