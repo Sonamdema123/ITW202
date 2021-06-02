ee79b61430060ab87ffb36d3f87802e9880d7206
Sonamdema123 <sdema1684@gmail.com>
2021-06-02 06:57:13 +0600

    TODO_20i,ii,iii

---
+++ Lab/TODO_20i/TODO_4/app/src/test/java/com/example/todo_4/ActivityInputOutputTest.java
@@ -0,0 +1,27 @@
+package com.example.todo_4;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.manipulation.Ordering;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ActivityInputOutputTest {
+
+
+//    @Test
+//    public void addition_isCorrect() {
+//        assertEquals(4, 2 + 2);
+//    }
+
+
+}
